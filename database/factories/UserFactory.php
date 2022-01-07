128d8949c5c8b412e68b134628b5e049d8db6a25
Jaweed Khan <mr.jaweedkhan@gmail.com>
2022-01-07 19:47:25 +0530

    faker user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 class UserFactory extends Factory
 {
@@ -15,8 +16,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'phone' => $this->faker->phoneNumber(),
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'profile_image' => 'http://via.placeholder.com/150x150',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
